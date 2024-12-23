f05190d84f462b0b2045e879ae73e5df6fdad406
Waleed Rafi <56467580+Waleed-Rafi@users.noreply.github.com>
2024-12-24 01:55:05 +0500

    fix: test route import removal, test cases for getJobsById controller

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,5 +1,4 @@
 import express from "express";
-import testRoutes from "./routes/test";
 import jobRoutes from "./routes/jobs";
 import { rateLimiter } from "./middlewares/rateLimiter";
 import { jobWorker } from "./workers/jobProcessor";
@@ -11,7 +10,6 @@ jobWorker();
 app.use(express.json());
 app.use(rateLimiter);
 
-app.use("/", testRoutes);
 app.use("/", jobRoutes);
 
 export default app;
